ed2e06ae55a86b59cf6ccde7da2e3212796b5419
Peter Oprea-Benko <15703843+obp3ter@users.noreply.github.com>
2019-08-06 13:26:43 +0300

    Initial commit

---
+++ src/main/java/msg/group/action/ActionApplication.java
@@ -0,0 +1,13 @@
+package msg.group.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionApplication.class, args);
+	}
+
+}
